8a00c70b9fe58ff8d2640f018d5b35651112643a
Quentin Brogly <qbrogly@gmail.com>
2020-09-16 08:49:07 -0700

    Update algorithms.js

--- algorithms.js
+++ algorithms.js
@@ -25,3 +25,33 @@ function sym(args) {
  
 sym([1, 2, 5], [2, 3, 5], [3, 4, 5]);
 // returns [1, 4, 5]
+
+
+// Return the number of total permutations of the provided string that don't have repeated consecutive letters. 
+// Assume that all characters in the provided string are each unique.
+
+function permAlone(str) {
+  function permute(str) {
+    if (str.length < 2) {
+      return str;
+    }
+    const permutations = [];
+    for (let i = 0; i < str.length; i++) {
+      const first = str[i];
+      const remaining = str.slice(0, i) + str.slice(i + 1);
+      const subPerms = permute(remaining);
+      for (let subPerm of subPerms) {
+        // don't add if 2 repeated consecutive letters
+        if (first !== subPerm[0]) {
+          permutations.push(first + subPerm);
+        }
+      }
+    }
+    return permutations;
+  }
+  const result = permute(str);
+  return result.length;
+}
+
+permAlone("aab");
+// returns 2
